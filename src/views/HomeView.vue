9a332e4435bbca96d03ac5777e3bf726644e06f9
tyjac <sans.gaming235@gmail.com>
2024-03-13 18:48:46 -0400

    new and imporved homepage

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,17 @@
 <template>
   <div class="container">
     <h1>Welcome to Shmordle!</h1>
+    <h2>RULES!</h2>
+    <P>You got 6 guesses on each one.</P>
+    <p>Grey = not in the word</p>
+    <p>Yellow = The right letter in the wrong place</p>
+    <p>Green = The correct letter in the correct place</p>
+    <h2>Game Modes</h2>
+    <p>DEFAULT - Standard game</p>
+    <p>BLUE - Standard game with unquie colors</p>
+    <p>THREE - Three letter games</p>
+    <p>FOUR - Four letter games</p>
+    <p>SIX - Six letter games</p>
   </div>
 </template>
 
